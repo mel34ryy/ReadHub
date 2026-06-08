57a0edc9955bfca90be9011e03ec258922758e9a
Mohamed Elashry <mohomed.1433@hotmail.com>
2026-06-08 08:13:40 +0300

    some changes on the books page

--- src/pages/Books.jsx
+++ src/pages/Books.jsx
@@ -62,12 +62,12 @@ function Books({ query, wishList, addToWishlist }) {
       </div>
       <section>
         <div className="flex justify-between mt-7">
-          <h3>Books for you!</h3>
+          <h3 className="text-3xl font-bold">Books for you!</h3>
           <div className="flex items-center gap-4">
             <select
               value={filter}
               onChange={(e) => setFilter(e.target.value)}
-              className="py-1.5 px-3"
+              className="py-1.5 px-3 text-xl border rounded outline-0"
               name="types"
               id="types"
             >
@@ -80,121 +80,129 @@ function Books({ query, wishList, addToWishlist }) {
           </div>
         </div>
         <div className="mt-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mb-12">
-          {isLoading
-            ? Array.from({ length: 4 }).map((_, index) => (
-                <BookSkeleton key={index} />
-              ))
-            : books.map((book, i) => {
-                const info = book.volumeInfo;
-                return (
-                  <Book
-                    key={book.id || i}
-                    id={book.id}
-                    title={info?.title || "No Title"}
-                    cover={
-                      info?.imageLinks?.thumbnail ||
-                      "https://placehold.co/150/dddddd/dddddd"
-                    }
-                    author={info?.authors?.[0] || "Unknown Author"}
-                    previewLink={book.accessInfo?.webReaderLink}
-                    pdfLink={book.accessInfo?.pdf?.downloadLink}
-                    isPdfAvailable={book.accessInfo?.pdf?.isAvailable}
-                    infoLink={info?.infoLink}
-                    saleability={book.saleInfo?.saleability}
-                    price={book.saleInfo?.listPrice?.amount}
-                    currency={book.saleInfo?.listPrice?.currencyCode}
-                    buyLink={book.saleInfo?.buyLink}
-                    wishList={wishList}
-                    addToWishlist={addToWishlist}
-                  />
-                );
-              })}
+          {isLoading ? (
+            Array.from({ length: 4 }).map((_, index) => (
+              <BookSkeleton key={index} />
+            ))
+          ) : books.length > 1 ? (
+            books.map((book, i) => {
+              const info = book.volumeInfo;
+              return (
+                <Book
+                  key={book.id || i}
+                  id={book.id}
+                  title={info?.title || "No Title"}
+                  cover={
+                    info?.imageLinks?.thumbnail ||
+                    "https://placehold.co/150/dddddd/dddddd"
+                  }
+                  author={info?.authors?.[0] || "Unknown Author"}
+                  previewLink={book.accessInfo?.webReaderLink}
+                  pdfLink={book.accessInfo?.pdf?.downloadLink}
+                  isPdfAvailable={book.accessInfo?.pdf?.isAvailable}
+                  infoLink={info?.infoLink}
+                  saleability={book.saleInfo?.saleability}
+                  price={book.saleInfo?.listPrice?.amount}
+                  currency={book.saleInfo?.listPrice?.currencyCode}
+                  buyLink={book.saleInfo?.buyLink}
+                  wishList={wishList}
+                  addToWishlist={addToWishlist}
+                />
+              );
+            })
+          ) : (
+            <p className="text-xl font-bold text-center">
+              there are no books (:
+            </p>
+          )}
         </div>
-        <div className="flex justify-center my-4">
-          <div>
-            <ul className="flex items-center justify-center gap-1">
-              <li>
-                <button
-                  disabled={currentPage === 1}
-                  className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200
+        {totalPages > 1 ? (
+          <div className="flex justify-center my-4">
+            <div>
+              <ul className="flex items-center justify-center gap-1">
+                <li>
+                  <button
+                    disabled={currentPage === 1}
+                    className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200
             ${
               currentPage === 1
                 ? "bg-(--color-pagination-disabled-bg) border-(--color-pagination-border) text-(--color-pagination-disabled-text) opacity-60 cursor-not-allowed"
                 : "bg-(--color-pagination-bg) border-(--color-pagination-border) text-(--color-pagination-text) hover:bg-(--color-pagination-active-bg) hover:text-(--color-pagination-active-text) hover:border-(--color-pagination-active-bg) cursor-pointer"
             }`}
-                  onClick={() =>
-                    setCurrentPage((prev) => Math.max(prev - 1, 1))
-                  }
-                >
-                  Previous
-                </button>
-              </li>
+                    onClick={() =>
+                      setCurrentPage((prev) => Math.max(prev - 1, 1))
+                    }
+                  >
+                    Previous
+                  </button>
+                </li>
 
-              {Array.from({ length: totalPages }, (_, i) => {
-                const page = i + 1;
-                const isActive = currentPage === page;
+                {Array.from({ length: totalPages }, (_, i) => {
+                  const page = i + 1;
+                  const isActive = currentPage === page;
 
-                if (
-                  page === 1 ||
-                  page === totalPages ||
-                  (page >= currentPage - 2 && page <= currentPage + 2)
-                ) {
-                  return (
-                    <li
-                      onClick={() => {
-                        if (page <= totalPages) setCurrentPage(page);
-                      }}
-                      key={page}
-                      className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200 cursor-pointer
+                  if (
+                    page === 1 ||
+                    page === totalPages ||
+                    (page >= currentPage - 2 && page <= currentPage + 2)
+                  ) {
+                    return (
+                      <li
+                        onClick={() => {
+                          if (page <= totalPages) setCurrentPage(page);
+                        }}
+                        key={page}
+                        className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200 cursor-pointer
           ${
             isActive
               ? "bg-(--color-pagination-active-bg) border-(--color-pagination-active-bg) text-(--color-pagination-active-text)"
               : "bg-(--color-pagination-bg) border-(--color-pagination-border) text-(--color-pagination-text) hover:bg-(--color-pagination-active-bg) hover:text-(--color-pagination-active-text) hover:border-(--color-pagination-active-bg)"
           }`}
-                    >
-                      <button
-                        disabled={isLoading || isActive}
-                        className="cursor-pointer"
                       >
-                        {page}
-                      </button>
-                    </li>
-                  );
-                }
+                        <button
+                          disabled={isLoading || isActive}
+                          className="cursor-pointer"
+                        >
+                          {page}
+                        </button>
+                      </li>
+                    );
+                  }
 
-                if (page === currentPage - 3 || page === currentPage + 3) {
-                  return (
-                    <li
-                      key={page}
-                      className="px-2 py-1.5 text-(--color-text-gray) text-sm"
-                    >
-                      ...
-                    </li>
-                  );
-                }
+                  if (page === currentPage - 3 || page === currentPage + 3) {
+                    return (
+                      <li
+                        key={page}
+                        className="px-2 py-1.5 text-(--color-text-gray) text-sm"
+                      >
+                        ...
+                      </li>
+                    );
+                  }
 
-                return null;
-              })}
+                  return null;
+                })}
 
-              <li>
-                <button
-                  disabled={isLoading || currentPage === totalPages}
-                  className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200
+                <li>
+                  <button
+                    disabled={isLoading || currentPage === totalPages}
+                    className={`px-3 py-1.5 rounded-md border text-sm font-medium transition-all duration-200
             ${
               currentPage === totalPages
                 ? "bg-(--color-pagination-disabled-bg) border-(--color-pagination-border) text-(--color-pagination-disabled-text) opacity-60 cursor-not-allowed"
                 : "bg-(--color-pagination-bg) border-(--color-pagination-border) text-(--color-pagination-text) hover:bg-(--color-pagination-active-bg) hover:text-(--color-pagination-active-text) hover:border-(--color-pagination-active-bg) cursor-pointer"
             }`}
-                  onClick={() =>
-                    setCurrentPage((prev) => Math.min(prev + 1, totalPages))
-                  }
-                >
-                  Next
-                </button>
-              </li>
-            </ul>
+                    onClick={() =>
+                      setCurrentPage((prev) => Math.min(prev + 1, totalPages))
+                    }
+                  >
+                    Next
+                  </button>
+                </li>
+              </ul>
+            </div>
           </div>
-        </div>
+        ) : null}
       </section>
     </div>
   );
